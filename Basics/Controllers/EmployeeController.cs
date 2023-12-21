6270c6a5ff8e09a095c23a851560823371e804f3
Can Yavuz <nasifcanyavuz@gmail.com>
2023-12-21 20:37:06 +0300

    first commit

---
+++ Basics/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+   public class EmployeeController : Controller
+   {
+        public IActionResult Index1()
+        {
+            String message = $"Hello World. {DateTime.Now.ToString()}";
+            return View("Index1", message);
+        }
+
+        public ViewResult Index2()
+        {
+            return View("index");
+        }
+
+        public IActionResult Index3(){
+            return Content("Employee");
+        }
+    
+   }
+}
